c1adb109e17dd712a3503ea2b6416dd729eee97b
hiapples <howard.2001@yahoo.com.tw>
2024-09-24 22:58:45 +0800

    6

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
     },
     pwa: {
       name: 'HANG OUT',
-      themeColor: '#000000',//浏览器工具栏的主题颜色
-      msTileColor: '#000000',//Windows 设备上磁贴的背景颜色
+      themeColor: 'rgba(41, 41, 41, 0.9)',//浏览器工具栏的主题颜色
+      msTileColor: 'rgba(41, 41, 41, 0.9)',//Windows 设备上磁贴的背景颜色
       appleMobileWebAppCapable: 'yes',//全屏模式显示
       appleMobileWebAppStatusBarStyle: 'black',//设置 iOS 设备上的状态栏的样式颜色
       workboxOptions: {
