2bba2d9a9c23a6ebc2da2446345d52900b313343
hiapples <howard.2001@yahoo.com.tw>
2024-10-21 16:57:50 +0800

    5

--- flask/app.py
+++ flask/app.py
@@ -35,34 +35,45 @@ client=pymongo.MongoClient("mongodb+srv://hiapples900:howard900@popalz.bm0pbdd.m
 db2=client.hangout
 collection=db2.users
 
+#cls db
+#result=collection.delete_many({})
 
 #首頁
 @app.route('/')
 def index():
     return render_template('index.html')
+
 #註冊頁面
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    msg=request.args.get("msg","")
+    return render_template('index.html',msg=msg)
+
 #登入頁面
 @app.route('/signin')
 def signin():
-    collection.insert_one({
-        "userid":"userid",
-        "password":"password"
-    })
-    return render_template('signin.html')
+    return render_template('index.html')
 
-##註冊程序
-@app.route("/signuping",methods=["POST"])
+# 註冊程序
+@app.route("/signuping", methods=["POST"])
 def signuping():
-    userid=request.form["signup_userid"]
-    password=request.form["signup_password"]
+    userid = request.form["signup_userid"]
+    password = request.form["signup_password"]
+
+    # 檢查用戶名是否已存在
+    existing_user = collection.find_one({"userid": userid})
+    if existing_user:
+        return redirect(url_for('signup', msg='此用戶名稱已被註冊！'))
+    
+    # 插入新用戶
     collection.insert_one({
-        "userid":userid,
-        "password":password
+        "userid": userid,
+        "password": password
     })
-    return redirect("/signin")
+    
+    return redirect(url_for('signin'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
