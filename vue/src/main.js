9fff0d990ff7353c7182034d46fd14ac40faa35b
hiapples <howard.2001@yahoo.com.tw>
2024-10-30 18:20:13 +0800

    11

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,13 +3,13 @@ import {createApp} from "vue";
 
 //載入根組件
 import App from "./app.vue";
-import router from './router/router.js'; 
+//import router from './router/router.js'; 
 
 
 //建立vue app物件
 const app = createApp(App);
 
-app.use(router); // 使用 router
+//app.use(router); // 使用 router
 
 //掛載到HTML標籤
 app.mount("#app");
